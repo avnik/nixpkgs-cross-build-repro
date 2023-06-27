dc6343128330ac1ab4c5170ff37b320f99c61e63
Alexander V. Nikolaev <avn@avnik.info>
2023-06-27 14:26:30 +0300

    WIP

--- flake.nix
+++ flake.nix
@@ -9,28 +9,30 @@
     outputs = { self, flake-parts, ...}@inputs:  flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" ];
       flake = { lib, ...}: {
-        nixosConfigurations = {
-          crossed = lib.nixosSystem {
-            system = "aarch64-linux";
-            modules = [
-              {
-                nixpkgs.hostPlatform.system = "aarch64-linux";
-                nixpkgs.buildPlatform.system = "x86_64-linux";
-              }
+        ghaf.nixos = [
               ({ pkgs, ...}: {
                 environment.systemPackages = with pkgs; [
                   # List problematic packages here
                   gnome.zenity
                 ];
-                boot.isContainer = true; # Don't build kernel and other slow things
               })
-            ];
-          };
+           ];
+        boards.fakeOrinBoard = {
+            system = "aarch64-linux";
+            nixos = [{
+                boot.isContainer = true; # Don't build kernel and other slow things
+            }];
+            flashScript = { image, pkgs, system, ...}: pkgs.runLocal "flashscript" { }
+              ''
+              '';
+
         };
       };
       perSystem = { self', inputs', system, pkgs, ... }: {
         packages = {
-         crossed = self.nixosConfigurations.crossed.config.system.build.toplevel;
+          # All packages should be derived  
+          fakeOrinBoard-cross-toplevel = self.nixosConfigurations.crossed.config.system.build.toplevel;
+          fakeOrinBoard-cross-flashscript = "";
         };
       };
     };
